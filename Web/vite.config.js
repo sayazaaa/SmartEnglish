e9192115cba30f4587650b58038e6a9f84669b77
ikklos <ikklo@qq.com>
2025-06-10 02:15:19 +0800

    env????

--- Web/vite.config.js
+++ Web/vite.config.js
@@ -5,14 +5,5 @@ import path from 'path';
 export default defineConfig({
   base: '/SmartEnglish/',
   plugins: [vue()],
-  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://175.178.5.83:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } }
 })
